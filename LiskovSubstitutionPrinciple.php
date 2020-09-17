4ecb035c1a9ab7293f6fbfcdcff75c2c7b28fe10
Rajesh Mondal <rajesh01749@gmail.com>
2020-09-17 22:29:05 +0600

    Liskov Substitution Principle

---
+++ LiskovSubstitutionPrinciple.php
@@ -0,0 +1,70 @@
+<?php
+abstract class Bird {
+    abstract function eat();
+    abstract function sleep();
+}
+
+abstract class FlyingBird extends Bird {
+    abstract function fly();
+}
+
+abstract class WalkingBird extends Bird {
+    abstract function walk();
+}
+
+class BirdManager {
+    function maintainBird( Bird $b ) {
+        $b->eat();
+        $b->sleep();
+    }
+
+    function moveFlyingBird( FlyingBird $fb ) {
+        $fb->fly();
+    }
+
+    function moveWalkingBird( WalkingBird $fb ) {
+        $fb->walk();
+    }
+}
+
+class Eagle extends FlyingBird {
+    function eat() {
+
+    }
+
+    function sleep() {
+
+    }
+
+    function fly() {
+
+    }
+}
+
+class Hawk extends WalkingBird {
+    function eat() {
+
+    }
+
+    function sleep() {
+
+    }
+
+    function walk() {
+        //walk the bird
+    }
+}
+
+class Penguine extends WalkingBird {
+    function eat() {
+
+    }
+
+    function sleep() {
+
+    }
+
+    function walk() {
+        //walk the bird
+    }
+}
